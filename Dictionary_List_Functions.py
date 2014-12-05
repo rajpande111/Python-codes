0df72da4d22b910a7b30ba08e9e711409259fdef
rajpande111 <rajpande111@hotmail.com>
2014-12-05 09:12:51 +0530

    Create Dictionary_List_Functions.py
    Different Datatypes Dictionaries,Lists and function in Python.

---
+++ Dictionary_List_Functions.py
@@ -0,0 +1,18 @@
+#Dictionary
+di = {'Raj':'Pvg','Avi':'Sinhgad','Shreemay':'Pccoe'}
+print di
+#Updation of Dictionary
+s = input("Enter new entry for Raj:")
+di['Raj'] = s
+#Lists
+li = ['raj','avi','shree','karan']
+print li
+#function
+n = input("Enter the value of n:")
+def mmm(n) :
+    print 'n=', n
+    if n>2 :
+           print 'a'
+    else :
+         print'b'
+mmm(n)
